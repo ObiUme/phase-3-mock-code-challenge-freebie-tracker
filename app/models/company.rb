da553d6c4a32089434896167a418668553bc8081
Obi <mr.obiume@gmail.com>
2021-08-19 11:03:59 -0700

    commit tree 7e2c2aa8ce48c6b0f6aaba52fb1efa1f951450fa

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,16 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
 
+    def give_freebie(dev, item_name, value)
+        self.freebies.create(company_id: self, dev_id: dev.id, item_name: item_name, value: value)
+    end
+
+    def self.oldest_company
+        self.all.where(founding_year: 1995)
+    end
+
+    # def self.oldest_company
+    #     Company.all.order(:founding_year).first
+    #   end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,12 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def recieved_one?(item_name)
+        self.freebies.pluck(:item_name).include?(item_name)
+    end
+
+    def give_away(dev, freebie)
+        freebie.update(dev_id: dev.id)
+    end
 end
